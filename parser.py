60c35d4664a1fc224a2378330d36c804d0cc1843
Evgeniy Kuzmin <d3tafaq@gmail.com>
2022-03-27 15:43:11 +0300

    re-work log processing and added csv output

--- parser.py
+++ parser.py
@@ -2,40 +2,15 @@ import sys
 import re
 import csv
 import os 
-import glob
 
-
-str_txt = '*.txt'
-str_end = '*.end'
-
-def get_log_files(path):
-    file_list = []
-    path = path + '/**/' + str_txt #ToDo: cange to another mathod
-    for file in (glob.glob(path, recursive=True)):
-        new_path = file[:-4] + str_end
-        if glob.glob(new_path): 
-            file_list.append(file)
-    return file_list
-
-def generate_report(files):
-    report = []
-    for file in files:
-        path_parsed = re.findall(r'\w+', file)
-        parameters = [item for item in path_parsed[-6:-2] if item != 'tests']
-        parameters.append(re.findall(r'..$', path_parsed[-2])[0])
-        parameters.append(get_pass_rate(file))
-        print(parameters)
-        report.append(parameters)
-
-    return report
-
-def generate_csv(report):
-
-    return
+report_header = ['OS', 'Architecture', 'Domain', 'Optimization', 'Pass Rate']
+supported_os = ['linux', 'windows', 'macosx']
+ext_txt = '.txt'
+ext_end = '.end'
 
 def get_pass_rate(filename):
 
-    with open(filename) as f:
+    with open(filename, 'r') as f:
         log = f.read()
         number_tests = 0
         success_tests = 0
@@ -48,23 +23,56 @@ def get_pass_rate(filename):
         else:
             return 'Aborted'
 
+
+def generate_report(path, os_list):
+
+    report = []
+    for os_type in os_list:
+        tests_path = os.path.join(path, os_type, 'tests')
+        for root, dirs, files in os.walk(tests_path):
+            for name in files:
+                log_file = os.path.splitext(name)
+                if ext_txt in log_file[1]:
+                    if log_file[0] + ext_end in os.listdir(root):
+                        pass_rate = get_pass_rate(os.path.join(root, name))
+                    else: 
+                        pass_rate = 'n/a'
+                    report.append([os_type] + re.findall(r'\w+', root.split(tests_path)[1]) + re.findall(r'..$',log_file[0]) + [pass_rate])
+    return report
+
+
+def generate_csv(report):
+
+    try:
+        with open('result.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(report_header)
+            writer.writerows(report)
+    except: 
+        print("CSV file cannot be created")
+        exit(1)
+    return
+
     
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Please, provide path to folder")
-        exit(1)
-    
-    os_type = "all"
-    path = sys.argv[1]
-    try:
-        os_type = sys.argv[2]
-        print("OS type is", os_type)
-    except:
-        print("OS type is not specified")
 
-    print("Specified path is", path, "selected OS is", os_type)
+    if len(sys.argv) >= 2: 
+        if not os.path.exists(sys.argv[1]): 
+            print("Path is not correct")
+            exit(1)
+        path = sys.argv[1]
+        if len(sys.argv) >= 3:
+            if sys.argv[2] not in supported_os:
+                print("OS is not correct")
+                exit(1)
+            elif sys.argv[2] not in os.listdir(path):
+                print("OS is not in folder")
+                exit(1)
+            os_list = [sys.argv[2]]
 
+        else: 
+            os_list = os.listdir(path)
 
-    generate_csv (generate_report (get_log_files(path)))
-
-
+        print("Specified path =", path, "selected OS = ", ', '.join(os_list))
+        generate_csv(generate_report(path, os_list))
+        print("Completed!")
